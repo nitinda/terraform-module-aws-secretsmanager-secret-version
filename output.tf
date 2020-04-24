74fc36c3e8d2b23f902e05177be2df6ca73a2a5b
notdefined <nitindas@vagrant.centos8.localdomain>
2020-04-24 17:39:57 +0100

    Modified Code

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "id" {
-  value = "aws_secretsmanager_secret_version.secretsmanager_secret_version.id"
+  value = aws_secretsmanager_secret_version.secretsmanager_secret_version.id
 }
 
 output "arn" {
-  value = "aws_secretsmanager_secret_version.secretsmanager_secret_version.arn"
+  value = aws_secretsmanager_secret_version.secretsmanager_secret_version.arn
 }
 
 output "version_id" {
-  value = "aws_secretsmanager_secret_version.secretsmanager_secret_version.version_id"
+  value = aws_secretsmanager_secret_version.secretsmanager_secret_version.version_id
 }
