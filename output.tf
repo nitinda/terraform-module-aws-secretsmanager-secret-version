1ef05bcd042b3a294b38a78981c4ccd8bbd9a3f2
nitinda <nitinda@gmail.com>
2020-04-27 10:39:16 +0100

    Modified output.tf

--- output.tf
+++ output.tf
@@ -10,6 +10,7 @@ output "version_id" {
   value = aws_secretsmanager_secret_version.secretsmanager_secret_version.version_id
 }
 
+
 output "secret_string" {
   value     = jsondecode(aws_secretsmanager_secret_version.secretsmanager_secret_version.secret_string)
   sensitive = true
